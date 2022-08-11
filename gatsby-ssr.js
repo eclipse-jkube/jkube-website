f94a21e2510142c7548b0fabe51cc645eb048901
Marc Nuri <marc@marcnuri.com>
2022-08-11 11:54:56 +0200

    fix: gatsby-ssr works in dev mode

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,10 @@
 const {defaultLangKey} = require('./src/i18n');
 
 exports.onRenderBody = ({pathname, setHtmlAttributes, loadPageDataSync}) => {
-  const {
-    result: {pageContext: {langKey}},
-  } = loadPageDataSync(pathname);
-  setHtmlAttributes({lang: langKey ? langKey : defaultLangKey});
+  if (process.env.NODE_ENV !== 'development') {
+    const {
+      result: {pageContext: {langKey}},
+    } = loadPageDataSync(pathname);
+    setHtmlAttributes({lang: langKey ? langKey : defaultLangKey});
+  }
 };
